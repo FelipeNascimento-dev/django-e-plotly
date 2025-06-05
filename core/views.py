14e3b34204112349312db1bff84640e150081fa8
FelipeNascimento-dev <felipe.nasci.dev@gmail.com>
2025-06-05 09:50:23 -0300

    view e url

--- core/views.py
+++ core/views.py
@@ -1,3 +1,18 @@
 from django.shortcuts import render
+import plotly.express as px
+from core.models import CO2
+
 
 # Create your views here.
+def chart(request):
+    co2 = CO2.objects.all()
+
+    fig = px.line(
+        x=[c.date for c in co2],
+        y=[c.value for c in co2],
+    )
+
+    chart = fig.to_html()
+
+    context = {'chart': chart}
+    return render(request, 'core/chart.html', context)
